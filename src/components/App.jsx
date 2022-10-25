f72ddb0e986d7406f89c4bd97a971f3f2fdd983b
Rostyslav Kononenko <rastikon@gmail.com>
2022-10-25 21:07:01 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,9 @@ import { Profile } from './Profile/Profile';
 import { Container, Item } from './App.styled';
 import { Statistics } from './Statistics/Statistics';
 import { Friends } from './Friends/Friends';
-import { upload } from '@testing-library/user-event/dist/upload';
-import friends from '../data/friends.json';
 import { Transactions } from './Transactions/Transactions';
 import transactions from '../data/transactions.json';
+import friends from '../data/friends.json';
 
 export const App = ({ data }) => {
   return (
